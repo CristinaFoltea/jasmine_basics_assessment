20dcf9f17eb536042f6a0b8c66cfd59c3baf2f19
Cristina Swearingen <criss.foltea@yahoo.com>
2015-06-04 00:53:11 -0600

    my tests and solutions for homework

---
+++ grader.js
@@ -0,0 +1,39 @@
+module.exports={
+  findTheMark: function(grades, n){
+   for(var i=0, init=""; i<grades.length; i++){
+      if(n > grades[i].range[0] && n < (grades[i].range[1])-1){
+        init=grades[i].grade;
+      }else if(n === grades[i].range[1] || n === grades[i].range[1]-1){
+        init=grades[i].grade + "+";
+    }else {init===grades[i].grade + "-";}
+  } return init;
+},
+  averageScore: function(grades){
+    var sum=grades.reduce(function(summ, elem){
+      return summ+elem;
+    }, 0);
+    return sum/grades.length;
+  },
+  medianScore: function(array){
+    var middle;
+    if (array.length % 2 !== 0){
+      middle=array[Math.floor(array.length/2)];
+    }else{
+      middle=(array[array.length/2] + array[array.length/2-1]) / 2;
+    }
+    return middle;
+  },
+  modeScore: function(array){
+    for (var i=0, countMax=0, valueMax; i<array.length; i++){
+      for(j=i, count=0; j<array.length; j++){
+        if (array[i] === array[j]){
+          count += 1;
+        }
+        if (count > countMax){
+          countMax = count;
+          valueMax = array[i];
+        }
+      }
+    } return valueMax;
+  }
+}
---
+++ spec/spec.js
@@ -0,0 +1,37 @@
+var grader=require('./../grader.js');
+var grades=[
+    {grade:"A", range:[90, 100]},
+    {grade:"B", range:[80, 89]},
+    {grade:"C", range:[70, 79]},
+    {grade:"D", range:[60, 69]},
+    {grade:"F", range:[0,  59]}
+  ];
+describe('#findTheMark() finds the corresponding the score ', function() {
+  it('given an array with objects find the corresponding grade', function() {
+ expect(grader.findTheMark(grades, 75)).toEqual("C");
+ })});
+
+ describe('#findTheMark() finds the corresponding the score ', function() {
+   it('Add \'+\' and \'-\' grades.  So a 99 is actually a A+ and an 80 is a B-', function() {
+  expect(grader.findTheMark(grades, 99)).toEqual("A+");
+  })});
+
+describe('#averageScore() finds the average score from a given array of scores', function() {
+  it('returns the average score', function() {
+ expect(grader.averageScore([90, 95, 87, 60])).toEqual(83);
+ })});
+
+describe('#medianScore() takes an array of test scores and calculates the middle one', function() {
+ it('returns the median score', function() {
+expect(grader.medianScore([90, 95, 90, 87, 60])).toEqual(90);
+})});
+
+describe('#medianScore() takes an array of test scores and calculates the middle one', function() {
+ it('if the number of scores is even return the average of the 2 middle ones', function() {
+expect(grader.medianScore([90, 95, 90, 80, 87, 60])).toEqual(85);
+})});
+
+describe('#medianScore() takes an array of test scores and calculates theone that appers most often', function() {
+ it('return the most frequent score', function() {
+expect(grader.modeScore([90, 95, 90, 80, 87, 60])).toEqual(90);
+})});
