6333966e20a6c664f2f922592405e51b13f9dc2a
Cristina Swearingen <criss.foltea@yahoo.com>
2015-06-05 07:10:19 -0600

    Changes the solution for mode and linted my code

--- grader.js
+++ grader.js
@@ -1,39 +1,36 @@
-module.exports={
-  findTheScore: function(grades, n){
-   for(var i=0, init=""; i<grades.length; i++){
-      if(n > grades[i].range[0] && n < (grades[i].range[1])-1){
-        init=grades[i].grade;
-      }else if(n === grades[i].range[1] || n === grades[i].range[1]-1){
-        init=grades[i].grade + "+";
-    }else {init===grades[i].grade + "-";}
-  } return init;
-},
-  averageScore: function(grades){
-    var sum=grades.reduce(function(summ, elem){
-      return summ+elem;
-    }, 0);
-    return sum/grades.length;
+module.exports = {
+  findTheScore: function (grades, n) {
+    for (var i = 0, init = ''; i < grades.length; i++) {
+      if (n > grades[i].range[0] && n < (grades[i].range[1]) - 1) {
+        init = grades[i].grade
+      }else if (n === grades[i].range[1] || n === grades[i].range[1] - 1) {
+        init = grades[i].grade + '+'
+      }else {init === grades[i].grade + '-'}
+    }return init
   },
-  medianScore: function(array){
-    var middle;
-    if (array.length % 2 !== 0){
-      middle=array[Math.floor(array.length/2)];
-    }else{
-      middle=(array[array.length/2] + array[array.length/2-1]) / 2;
-    }
-    return middle;
+  averageScore: function (grades) {
+    var sum = grades.reduce(function (summ, elem) {return summ + elem}, 0)
+    return sum / grades.length
   },
-  modeScore: function(array){
-    for (var i=0, countMax=0, valueMax; i<array.length; i++){
-      for(j=i, count=0; j<array.length; j++){
-        if (array[i] === array[j]){
-          count += 1;
-        }
-        if (count > countMax){
-          countMax = count;
-          valueMax = array[i];
-        }
-      }
-    } return valueMax;
-  }
+  medianScore: function (array) {
+    var middle
+    if (array.length % 2 !== 0) {
+      middle = array[Math.floor(array.length / 2)]
+    }else {middle = (array[array.length / 2] + array[array.length / 2 - 1]) / 2}
+    return middle
+  },
+modeScore: function (array) {
+  var max = 0
+  var result = array.reduce(function (init, value) {
+    if (init.hasOwnProperty(value)) {
+      init[value] += 1
+      if (max < init[value]) { max = init[value]}
+    }else {init[value] = 1}
+    return init
+  }, {})
+  var endArr = Object.keys(result).filter(function (value) { return result[value] === max})
+  if (endArr.length > 1) {
+    return endArr
+  } else {return endArr[0]}
+}
 }
--- spec/spec.js
+++ spec/spec.js
@@ -1,19 +1,19 @@
 var grader=require('./../grader.js');
 var grades=[
-    {grade:"A", range:[90, 100]},
-    {grade:"B", range:[80, 89]},
-    {grade:"C", range:[70, 79]},
-    {grade:"D", range:[60, 69]},
-    {grade:"F", range:[0,  59]}
+    {grade:'A', range:[90, 100]},
+    {grade:'B', range:[80, 89]},
+    {grade:'C', range:[70, 79]},
+    {grade:'D', range:[60, 69]},
+    {grade:'F', range:[0,  59]}
   ];
 describe('#findTheScore() finds the corresponding the score ', function() {
   it('given an array with objects find the corresponding grade based on the score', function() {
- expect(grader.findTheScore(grades, 75)).toEqual("C");
+ expect(grader.findTheScore(grades, 75)).toEqual('C');
  })});
 
  describe('#findTheScore() strech finds the corresponding the score ', function() {
    it('Add \'+\' and \'-\' grades.  So a 99 is actually a A+ and an 80 is a B-', function() {
-  expect(grader.findTheScore(grades, 99)).toEqual("A+");
+  expect(grader.findTheScore(grades, 99)).toEqual('A+');
   })});
 
 describe('#averageScore() finds the average score from a given array of scores', function() {
@@ -31,7 +31,7 @@ describe('#medianScore() takes an array of test scores and calculates the middle
 expect(grader.medianScore([90, 95, 90, 80, 87, 60])).toEqual(85);
 })});
 
-describe('#medianScore() takes an array of test scores and calculates the one that appers most often', function() {
+describe('#modeScore() takes an array of test scores and calculates the one that appers most often', function() {
  it('return the most frequent score', function() {
-expect(grader.modeScore([90, 95, 90, 80, 87, 60])).toEqual(90);
+expect(grader.modeScore([90, 95, 90, 87, 80])).toEqual('90');
 })});
